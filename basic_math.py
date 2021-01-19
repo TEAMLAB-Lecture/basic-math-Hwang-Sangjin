9a5530e21d34d19c12853e5de3dab8f50ca91c15
Hwang-Sangjin <aosdb5640@naver.com>
2021-01-19 19:05:44 +0900

    Update my code

--- basic_math.py
+++ basic_math.py
@@ -126,7 +126,7 @@ def get_median(number_list):
 
     else:
         center = int(list_num/2)
-        if center%2==0:
+        if center%2==1:
             median = number_list[center]
         else:
             median = (number_list[center-1] + number_list[center])/2
